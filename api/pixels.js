3ec0078613310af1781aa7e1459bfc2829a409b6
Quinten <35972902+quinten135@users.noreply.github.com>
2026-03-29 17:22:11 +0200

    Update pixels.js

--- api/pixels.js
+++ api/pixels.js
@@ -1,11 +1,11 @@
 import Jimp from 'jimp';
 
 export default async function handler(req, res) {
-    const url = req.query.url || 'https://picsum.photos/1920/1080';
-    const w = parseInt(req.query.w) || 1920;
-    const h = parseInt(req.query.h) || 1080;
+    const url = req.query.url || 'https://picsum.photos/854/480';
+    const w = parseInt(req.query.w) || 854;
+    const h = parseInt(req.query.h) || 480;
     const chunkY = parseInt(req.query.chunkY) || 0;
-    const chunkH = parseInt(req.query.chunkH) || 108; // 10 chunks van 108 rijen
+    const chunkH = parseInt(req.query.chunkH) || 48;
 
     const image = await Jimp.read(url);
     image.resize(w, h);
@@ -15,7 +15,8 @@ export default async function handler(req, res) {
         pixels.push(this.bitmap.data[idx] / 255)
         pixels.push(this.bitmap.data[idx+1] / 255)
         pixels.push(this.bitmap.data[idx+2] / 255)
+        pixels.push(this.bitmap.data[idx+3] / 255) // alpha ook mee voor EditableImage
     });
 
-    res.json({ pixels, w, chunkY, chunkH });
+    res.json({ pixels, w, h, chunkY, chunkH });
 }
